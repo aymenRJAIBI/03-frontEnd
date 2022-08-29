29103108831edca5bc88b44d1aff47af6f42764e
aymenRJAIBI <aymenrjaibi23@gmail.com>
2022-08-29 16:18:42 +0200

    afficher produit liste et ajout de bootstrap

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
+  //templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    console.log("Test1");
+    this.listProducts();
+
   }
 
+  listProducts() {
+    console.log("Test2");
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+
+    )
+    console.log(this.products);
+  }
 }
